00a1473340674604e540fb4da45ae369a60b2c43
insukeom <75593913+insukeom@users.noreply.github.com>
2020-12-07 15:37:58 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -35,7 +35,9 @@ print(df.shape[0])
 #siakrGoogleApi.googleSentTransIter(df, title_col='title', content_col='Mention Content', text_col='textNorm', id_col='sia_id', title_save_col='titleEn', content_save_col='contentEn', trans_save_col='textEn', sentp_save_col='Sentiment_point', sent_save_col='Sentiment', save_path=project_path + '/data/trans/', file_name='dorco_vietnam_trans', target_language='en', interval=1000)
 #siakrGoogleApi.googleSentTransIter_survey(df, content_col='content', id_col='sia_id', trans_save_col='contentEn', sentp_save_col='Sentiment_point', sent_save_col='Sentiment', save_path=project_path + '/data/survey/trans/', file_name='dorco_thailand_trans', target_language='en', interval=1000)
 
+siakrGoogleApi.googleSentTransIter(df, title_col='title', content_col='Mention Content', text_col='textNorm', id_col='sia_id', title_save_col='titleEn', content_save_col='contentEn', trans_save_col='textEn', sentp_save_col='Sentiment_point', sent_save_col='Sentiment', save_path=project_path + '/data/trans/', file_name='dorco_vietnam_trans', target_language='en', interval=1000)
 siakrGoogleApi.googleSentTransIter_survey(df, content_col='content', id_col='sia_id', trans_save_col='contentEn', sentp_save_col='Sentiment_point', sent_save_col='Sentiment', save_path=project_path + '/data/survey/trans/', file_name='dorco_thailand_trans', target_language='en', interval=1000)
 
 final_df = siakr_common.read_source_files_from_folder(path=project_path + '/data/trans/', ext='csv', sep=';')
 final_df.to_csv(save_file, encoding='utf-8-sig', sep=';')
+
